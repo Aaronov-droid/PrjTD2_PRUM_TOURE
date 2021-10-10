53abd51de6f5fadaf03f15803548075e7fdd14da
AisuShimasu <59924732+AisuShimasu@users.noreply.github.com>
2021-10-11 01:13:58 +0200

    Add files via upload
    Add ListeMemoireAbonnement / ListeMemoireRevue, need finishing and correcting error

--- cpoa_td2/src/daoListeMemoire/ListeMemoireAbonnementDAO.java
+++ cpoa_td2/src/daoListeMemoire/ListeMemoireAbonnementDAO.java
@@ -14,8 +14,9 @@ public class ListeMemoireAbonnementDAO implements AbonnementDAO{
 	private ListeMemoireAbonnementDAO() 
 	{
 		this.donnees = new ArrayList<Abonnement>();
-		this.donnees.add(new Abonnement(1,datedebut,datefin,1,1));
-		this.donnees.add(new Abonnement(2,datedebut,datefin,2,2));
+		
+		//this.donnees.add(new Abonnement(1,datedebut,datefin,1,1));
+		//this.donnees.add(new Abonnement(2,datedebut,datefin,2,2));
 	}
 	
 	public static ListeMemoireAbonnementDAO getInstance() {
@@ -30,7 +31,7 @@ public class ListeMemoireAbonnementDAO implements AbonnementDAO{
 	@Override
 	public Abonnement getById(int id) {
 		// TODO Auto-generated method stub
-		int idx = this.donnees.indexOf(new Abonnement("code_postal", id, "nom", "prenom", "N°rue", "voie", "ville", "pays"));
+		//int idx = this.donnees.indexOf(new Abonnement(id," "));
 		if (idx == -1)
 		{
 			throw new IllegalArgumentException("Aucun client ne possède cet identifiant");
@@ -44,19 +45,51 @@ public class ListeMemoireAbonnementDAO implements AbonnementDAO{
 	@Override
 	public boolean create(Abonnement objet) {
 		// TODO Auto-generated method stub
-		return false;
+		objet.setId_abonnement(3);
+		while (this.donnees.contains(objet))
+		{
+			objet.setId_abonnement(objet.getId_abonnement() + 1);
+		}
+		boolean ok = this.donnees.add(objet);
+		return ok;
 	}
 
 	@Override
 	public boolean update(Abonnement objet) {
 		// TODO Auto-generated method stub
-		return false;
+		int idx = this.donnees.indexOf(objet);
+		if (idx == -1)
+		{
+			throw new IllegalArgumentException("Tentative de modification d'un abonnement inexistant");
+		}
+		else
+		{
+			this.donnees.set(idx, objet)
+		}
+		return true;
 	}
 
 	@Override
 	public boolean delete(Abonnement objet) {
 		// TODO Auto-generated method stub
-		return false;
+		Abonnement supprime;
+		
+		int idx = this.donnees.indexOf(objet);
+		if (idx == -1)
+		{
+			throw new IllegalArgumentException("Tentative de suppression d'un abonnement inexistant");
+		}
+		else
+		{
+			supprime=this.donnees.remove(idx);
+		}
+		return objet.equals(supprime);
+	}
+	
+	@Override
+	public ArrayList<Abonnement> findAll()
+	{
+		return (ArrayList<Abonnement>) this.donnees;
 	}
 
 }
--- cpoa_td2/src/daoListeMemoire/ListeMemoireRevueDAO.java
+++ cpoa_td2/src/daoListeMemoire/ListeMemoireRevueDAO.java
@@ -5,8 +5,15 @@ import metierDAO.RevueDAO;
 
 public class ListeMemoireRevueDAO implements RevueDAO {
 	private static ListeMemoireRevueDAO instance;
+	private ArrayList<Revue> donnees;
 	
-	private ListeMemoireRevueDAO() {}
+	private ListeMemoireRevueDAO() 
+	{
+		this.donnees = new ArrayList<Revue>();
+		
+		//this.donnees.add(new Revue(1,...));
+		//this.donnees.add(new Revue(2,...));
+	}
 	
 	public static ListeMemoireRevueDAO getInstance() {
 	if (instance==null) {
@@ -18,25 +25,65 @@ public class ListeMemoireRevueDAO implements RevueDAO {
 	@Override
 	public Revue getById(int id) {
 		// TODO Auto-generated method stub
-		return null;
+		//int idx = this.donnees.indexOf(new Revue(""));
+		if (idx == -1)
+		{
+			throw new IllegalArgumentException("Aucune revue ne possède cet identifiant");
+		}
+		else
+		{
+			return this.donnees.get(idx);
+		}
 	}
 
 	@Override
 	public boolean create(Revue objet) {
 		// TODO Auto-generated method stub
-		return false;
+		objet.setId_revue(3);
+		while (this.donnees.contains(objet))
+		{
+			objet.setId_revue(objet.getId_revue() + 1);
+		}
+		boolean ok = this.donnees.add(objet);
+		return ok;
 	}
 
 	@Override
 	public boolean update(Revue objet) {
 		// TODO Auto-generated method stub
-		return false;
+		int idx = this.donnees.indexOf(objet);
+		if (idx == -1)
+		{
+			throw new IllegalArgumentException("Tentative de modification d'une revue inexistante");
+		}
+		else
+		{
+			this.donnees.set(idx, objet)
+		}
+		return true;
 	}
 
 	@Override
 	public boolean delete(Revue objet) {
 		// TODO Auto-generated method stub
-		return false;
+		Revue supprime;
+		
+		int idx = this.donnees.indexOf(objet);
+		if (idx == -1)
+		{
+			throw new IllegalArgumentException("Tentative de suppression d'une revue inexistante");
+		}
+		else
+		{
+			supprime=this.donnees.remove(idx);
+		}
+		return objet.equals(supprime);
+	}
+	
+	@Override
+	public ArrayList<Revue> findAll()
+	{
+		return (ArrayList<Revue>) this.donnees;
 	}
 
 }

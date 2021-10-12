e1b28c3f9c44fd862f87b4680709c35881b32830
Matis <74223532+Aaronov-droid@users.noreply.github.com>
2021-10-12 18:34:24 +0200

    Add files via upload

---
+++ cpoa_td2/src/td3/Normaliser.java
@@ -0,0 +1,32 @@
+package td3;
+
+public interface Normaliser {
+	
+	public static String supprEspaces(String ch) {
+		
+		return ch;
+	}
+	
+	public static String normaliserPays(String pays) {
+		
+		return pays;
+	}
+	
+	public static String normaliserVille(String ville) {
+		
+		return ville;
+	}
+
+	public static String normaliserCodePostale(String code_postal) {
+	
+	return code_postal;
+}
+
+	public static String normaliserVoie(String voie) {
+	
+	return voie;
+}
+	
+	
+
+}

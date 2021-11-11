e429c7a4b007d2515ee539feb6873916e8d0010d
AisuShimasu <59924732+AisuShimasu@users.noreply.github.com>
2021-11-11 15:29:00 +0100

    Update ListeAbonnement.java
    Change

--- Projet/ListeAbonnement.java
+++ Projet/ListeAbonnement.java
@@ -1,20 +1,20 @@
 public class ListeAbonnement implements Initializable {
 
     @FXML
-    private TableView<ListeAbonnement> TableAbonnement;
+    private TableView<Abonnement> TableAbonnement;
     @FXML
     private Button ButtonAjoutAbonnement, ButtonSuppAbonnement, ButtonAcceuil;
 
     public void setColumns() //Création des colonnes
     {
-        TableColumn<TableAbonnement, String> colClient = new TableColumn<>("Client");
-        TableColumn<TableAbonnement, String> colRevue = new TableColumn<>("Revue");
-        TableColumn<TableAbonnement, String> colDebut = new TableColumn<>("Date de début");
-        TableColumn<TableAbonnement, String> colFin = new TableColumn<>("Date de fin");
-        colClient.setCellValueFactory(new PropertyValueFactory<TableAbonnement, String>("Client"));
-        colRevue.setCellValueFactory(new PropertyValueFactory<TableAbonnement, String>("Revue"));
-        colDebut.setCellValueFactory(new PropertyValueFactory<TableAbonnement, String>("Debut"));
-        colFin.setCellValueFactory(new PropertyValueFactory<TableAbonnement, String>("Fin"));
+        TableColumn<Abonnement, String> colClient = new TableColumn<>("Client");
+        TableColumn<Abonnement, String> colRevue = new TableColumn<>("Revue");
+        TableColumn<Abonnement, String> colDebut = new TableColumn<>("Date de début");
+        TableColumn<Abonnement, String> colFin = new TableColumn<>("Date de fin");
+        colClient.setCellValueFactory(new PropertyValueFactory<Abonnement, String>("Client"));
+        colRevue.setCellValueFactory(new PropertyValueFactory<Abonnement, String>("Revue"));
+        colDebut.setCellValueFactory(new PropertyValueFactory<Abonnement, String>("Debut"));
+        colFin.setCellValueFactory(new PropertyValueFactory<Abonnement, String>("Fin"));
         this.TableAbonnement.getColumns().setAll(colClient, colRevue, colDebut, colFin);
     }
 
